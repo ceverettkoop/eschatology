f48f241da272d70f188fb4ccfea0723223365153
Sean McBride <brennsto@gmail.com>
2024-03-20 21:37:16 -0700

    wip

--- src/entity.h
+++ src/entity.h
@@ -6,7 +6,9 @@
 typedef uint64_t EntityID;
 
 #define NEW_ENTITY_FUNC_NAME(type) init_##type
-#define NEW_ENTITY_DECL(gs, ...) /
+#define NEW_ENTITY_DECL(gs, ...) 
+    EntityID NEW_ENTITY_FUNC_NAME(GameState* gs, )
+
 
 #define NEW_ENTITY_DEF(gs, ...) /
     EntityID new_id = NEW_ENTITY_FUNC_NAME(gs);
